0f81937625348b4edd489f7e37935c71af28240c
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-10-14 13:53:25 +0200

    Added trace

---
+++ test/trace.ts
@@ -0,0 +1,31 @@
+import 'mocha'
+import { expect } from 'chai'
+
+import { trace } from '../src/trace'
+
+describe('trace', () => {
+  it('logs its arguments to console', () => {
+    const [message] = captureConsole(() => {
+      trace('x')(1)
+    })
+    expect(message).to.equal('x: 1')
+  })
+
+  it('returns its argument', () => {
+    let result
+    captureConsole(() => {
+      result = trace('x')(1)
+    })
+
+    expect(result).to.equal(1)
+  })
+})
+
+function captureConsole (fn: Function) {
+  const log = console.log
+  let messages: string[] = []
+  console.log = (x: any) => messages.push('' + x)
+  fn()
+  console.log = log
+  return messages
+}
