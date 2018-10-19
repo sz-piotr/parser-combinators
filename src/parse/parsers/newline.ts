e46705c23e33f434edbb223d4d639b07e1bc9c78
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-10-19 14:28:38 +0200

    Simplified newline parser

--- src/parse/parsers/newline.ts
+++ src/parse/parsers/newline.ts
@@ -3,14 +3,14 @@ import { Input, Parser } from '../types'
 
 const expected = ['newline']
 export const newline: Parser<string> = (input: Input) => {
-  const char = input.get()
-  if (char === '\n') {
-    return new Right({ value: char, input: input.advance(1) })
-  } else if (char === '\r') {
+  const value = input.get()
+  if (value === '\n') {
+    return new Right({ value, input: input.advance(1) })
+  } else if (value === '\r') {
     if (input.get(1) === '\n') {
       return new Right({ value: '\r\n', input: input.advance(2) })
     } else {
-      return new Right({ value: char, input: input.advance(1) })
+      return new Right({ value, input: input.advance(1) })
     }
   } else {
     return new Left({ expected, input })
