f6e609dfb4c75d236d6097f6fb0bc10431a7a28b
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-10-19 17:17:21 +0200

    Updated endOfInput to not call advance

--- src/parse/parsers/endOfInput.ts
+++ src/parse/parsers/endOfInput.ts
@@ -4,5 +4,5 @@ import { Input, Parser } from '../types'
 const expected = ['end of input']
 export const endOfInput: Parser<undefined> = (input: Input) =>
   input.get() === undefined
-    ? new Right({ value: undefined, input: input.advance(1) }) // TODO: no advance?
+    ? new Right({ value: undefined, input })
     : new Left({ expected, input })
